5f8c1f81d9758989d3a469c93a33b1b385647316
Minhhieu1001 <hieuminhpham1001@gmail.com>
2025-04-09 21:45:33 +0700

    5. isActive-NavLink  + 6. React Router + 7. SPA -Single Application Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,43 @@
 
+
 import Header from "./component/Header"
 import Siderbar from "./component/Siderbar"
+import Content from "./component/Content"
+import { BrowserRouter,Routes,Route } from "react-router-dom";
+import AnaLitics from "./pages/Analytics";
+import Project from "./pages/Project";
+import Team from "./pages/Team";
+import Messager from "./pages/Messager";
+import Intergations from "./pages/Intergations";
+import Fount404 from "./pages/Fount404";
 import Footer from "./component/Footer";
-import Content from "./component/Content";
+
+
+
+
 
 function App() {
 
   return (
+    <BrowserRouter>
       <div className="w-full">
+       
           <div className="grid grid-cols-12 ">
             <div className="col-span-2 shadow-2xl">
             <Siderbar />
             </div>
             <div className="col-span-10 shadow-lg">         
             <Header />
-            <Content />
+              <Routes>
+                <Route path="/" element={<Content />} />
+                <Route path="/dashboar" element={<Content />} />
+                <Route path="/analytis" element={<AnaLitics />} />
+                <Route path="/project" element={<Project />} />
+                <Route path="/team" element={<Team />} />
+                <Route path="/messager" element={<Messager />} />
+                <Route path="/intergations" element={<Intergations />} />
+                <Route path="/*" element={<Fount404 />} />
+              </Routes>
             </div>
           </div>
          <br />
@@ -26,6 +49,7 @@ function App() {
           </div>
            
       </div>
+      </BrowserRouter>
   )
 }
 
--- src/component/Siderbar.jsx
+++ src/component/Siderbar.jsx
@@ -13,45 +13,45 @@ const Sidebar = () => {
       <div>
         <ul>
           <li className="rounded ml-5 p-1 bg-gray-100">
-            <div to="/dashboar" className="flex items-center p-2 rounded font-bold no-underline w-full bg-pink-500 text-white">
+            <NavLink to="/dashboar" className={navLinkClass}>
               <img src="./Squares four 1.png" alt="" />
               <h4 className="ml-5">Dashboar</h4>
-            </div>
+            </NavLink>
           </li>
 
           <li className="rounded ml-5 p-1 bg-gray-100">
-            <div to="/project" className="flex p-2 font-bold">
+            <NavLink to="/project" className={navLinkClass}>
               <img src="./Folder.png" alt="" />
               <h4 className="ml-5">Project</h4>
-            </div>
+            </NavLink>
           </li>
 
           <li className="rounded ml-5 p-1 bg-gray-100">
-            <div to="/team" className="flex p-2 font-bold">
+            <NavLink to="/team" className={navLinkClass}>
               <img src="./Groups.png" alt="" />
               <h4 className="ml-5">Team</h4>
-            </div>
+            </NavLink>
           </li>
 
           <li className="rounded ml-5 p-1 bg-gray-100">
-            <div to="/analytis" className="flex p-2 font-bold">
+            <NavLink to="/analytis" className={navLinkClass}>
               <img src="./Pie chart.png" alt="" />
               <h4 className="ml-5">Analytis</h4>
-            </div>
+            </NavLink>
           </li>
 
           <li className="rounded ml-5 p-1 bg-gray-100">
-            <div to="/messager" className="flex p-2 font-bold">
+            <NavLink to="/messager" className={navLinkClass}>
               <img src="./Chat.png" alt="" />
               <h4 className="ml-5">Messager</h4>
-            </div>
+            </NavLink>
           </li>
 
           <li className="rounded ml-5 p-1 bg-gray-100">
-            <div to="/intergations" className="flex p-2 font-bold">
+            <NavLink to="/intergations" className={navLinkClass}>
               <img src="./Code.png" alt="" />
               <h4 className="ml-5">Intergations</h4>
-            </div>
+            </NavLink>
           </li>
           <br />
         </ul>
