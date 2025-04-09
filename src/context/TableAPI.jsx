fc4e140c8ccab871d63380597e2233d0f70b23cd
Minhhieu1001 <hieuminhpham1001@gmail.com>
2025-04-09 22:42:00 +0700

    9. Modal connect API (get, put)

--- src/context/TableAPI.jsx
+++ src/context/TableAPI.jsx
@@ -1,26 +1,29 @@
 
 import { createContext, useState, useEffect, useContext } from "react";
 import { toast } from "react-toastify";
+
 const TableContext = createContext();
 
 export const TableProvider = ({ children }) => {
   const [dataTB, setData] = useState([]);
   const API_URL = "https://67c865040acf98d070866108.mockapi.io/user"
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch(API_URL);
-        const data = await response.json();
-        setData(data);
-      } catch (error) {
-        console.error("Fetch error:", error);
-        toast.error("Failed to fetch data");
-      }
-    };
 
-    fetchData();
-  }, []);
 
+  //GET
+useEffect(() => {
+  fetch(API_URL)
+    .then((res) => res.json())     
+    .then((data) => {
+      setData(data);        
+      console.log(data);    
+    })
+    .catch((error) => {
+      console.error("Error fetching data:", error);
+      toast.error("Lấy dữ liệu thất bại. Vui lòng thử lại!");
+    });
+}, []);
+
+  //PUT
   const HandleUpdateUser = async (updatedUser,index) => {
     try {
       const response = await fetch(`${API_URL}/${index}`, {
@@ -45,10 +48,9 @@ export const TableProvider = ({ children }) => {
 
   const totalUser = dataTB.length;
 
- 
   return (
     <TableContext.Provider
-      value={{ dataTB,totalUser,HandleUpdateUser}}
+      value={{ dataTB, HandleUpdateUser, totalUser }}
     >
       {children}
     </TableContext.Provider>
